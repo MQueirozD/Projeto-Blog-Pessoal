a751d04fc43800b90b1dc993295f415dde29cf6a
Matues <mateus.q.d@gmail.com>
2022-08-31 16:18:15 -0300

    Versão 1.1

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,27 +6,36 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+
+//indica ao spring q o objeto vai ser uma tabela no banco de dados
 @Entity
+/*dar o nome da tabale a ser criada.
+Sem ela a tabela é criada com o nome objeto*/
 @Table(name = "tb_postagens")
 
 public class Postagem {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id //indica q o id da tabela vai sei chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//seria o auto_increment do mysql 
 	private Long id;
 	
+	//@NotBlank não deixar que o compo comece com espaço
 	@NotBlank(message = "O atributo titulo é Obrigatorio!")
 	@Size(min=5,max=1000, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
 	private String titulo;
 	
+	//@NotNull não deixa que o o campo fica vazio
+	@NotNull
 	@NotBlank(message="O atributo texto é obrigatório!")
 	@Size(min=10,max=1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 100 caracteres")
 	private String texto;
 	
 	@UpdateTimestamp
+	//localDateTime = vai me passar o data e o horario automaticamente qnd for postar
 	private LocalDateTime data;
 	
 
--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,10 +1,22 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.generation.blogpessoal.model.Postagem;
 
 @Repository
+/*JPA>Biblioteca de métodos e instruções que descreve como deve ser o comportamento dos frameworks.*/
+//JPA - é que vai ligar entre o banco de dados e o back_end
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+	//seria no mysql = SELECT *FROM tb_postagem WHERE titulo LIKE "%%";
+	//ajuda a procurar o que vc quer
+	//public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
 
 }
+// findAllByTituloContainingIgnoreCase
